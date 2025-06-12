0f309233cd85f1ba54e1a47ecdec73acf7cb852d
HelgaZhizhka <helgazhyzhka@gmail.com>
2025-06-12 17:04:58 +0200

    fix: refactor settings for supabase client; add types

--- src/shared/api/supabase-client/index.ts
+++ src/shared/api/supabase-client/index.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
+import type { Database } from '@shared/api/database.types';
 import { config } from '@shared/config';
 
 const { SUPABASE_URL, SUPABASE_ANON_KEY } = config;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
