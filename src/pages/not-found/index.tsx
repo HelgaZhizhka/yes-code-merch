382b879133c4f84c916e0ba6a1cdc5cbe64990f0
Angelina Bezik <angelina1337bz@gmail.com>
2025-06-17 17:19:16 +0300

    feat: implement 404 page

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,8 +1,35 @@
+import { Link } from '@tanstack/react-router';
+
+import ErrorImage from '@shared/assets/404.png';
+import { ROUTES } from '@shared/routing/types';
+import { Button } from '@shared/ui/button';
+
+import styles from './not-found.module.css';
+
 export const NotFound = (): React.JSX.Element => {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl">404 page not found</h1>
-      <p>This page does not exist</p>
+    <div className="flex flex-col items-center justify-center min-h-screen gap-2">
+      <div className="relative flex items-center justify-center">
+        <h1
+          className={`absolute bottom-15 text-9xl sm:text-[15rem] md:text-[20rem] lg:text-[30rem] text-primary ${styles['animate-glitch']}`}
+        >
+          404
+        </h1>
+        <img
+          src={ErrorImage}
+          alt="Not found"
+          className="z-10 w-64 sm:w-100 md:w-130 lg:w-190"
+        />
+      </div>
+      <h2 className="text-center">
+        <span className="text-accent">WHOOPS! </span>Page they&apos;re looking
+        for could not be found.
+      </h2>
+      <Button size="lg">
+        <Link to={ROUTES.HOME} className="text-primary-foreground text-xl">
+          Back to main
+        </Link>
+      </Button>
     </div>
   );
 };
