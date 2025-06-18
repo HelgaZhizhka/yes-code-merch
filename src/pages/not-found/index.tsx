82ed88927a87fc96ffcdcd7011f91bb3e049b5ec
Angelina Bezik <angelina1337bz@gmail.com>
2025-06-18 16:54:54 +0300

    fix: update headings

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -10,21 +10,21 @@ export const NotFound = (): React.JSX.Element => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-2">
       <div className="relative flex items-center justify-center">
-        <h1
+        <h2
           className={`absolute bottom-15 text-9xl sm:text-[15rem] md:text-[20rem] lg:text-[30rem] text-primary ${styles['animate-glitch']}`}
         >
           404
-        </h1>
+        </h2>
         <img
           src={ErrorImage}
           alt="Not found"
           className="z-10 w-64 sm:w-100 md:w-130 lg:w-190"
         />
       </div>
-      <h2 className="text-center">
+      <h3 className="text-center">
         <span className="text-accent">WHOOPS! </span>Page they&apos;re looking
         for could not be found.
-      </h2>
+      </h3>
       <Button size="lg">
         <Link to={ROUTES.HOME} className="text-primary-foreground text-xl">
           Back to main
