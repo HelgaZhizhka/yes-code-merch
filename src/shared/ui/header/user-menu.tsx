18363a5c0b8193247cf238947eeb2b7c8865860f
pambaka <pambaka@ya.ru>
2025-06-19 09:17:19 +0200

    refactor: add profile icon, refactor user menu

--- src/shared/ui/header/user-menu.tsx
+++ src/shared/ui/header/user-menu.tsx
@@ -1,36 +1,43 @@
 import { ROUTES } from '@/shared/routing/types';
 import { Link } from '@tanstack/react-router';
 import { Button } from '@shared/ui/button';
-import { ShoppingCart } from 'lucide-react';
+import { ShoppingCart, User } from 'lucide-react';
 
 export const UserMenu = (): React.JSX.Element => {
   const isAuthenticated = false;
 
   return (
     <nav className="flex gap-4 text-inherit">
-      {isAuthenticated && <Link to={ROUTES.PROFILE}>Profile</Link>}
-      {!isAuthenticated && (
-        <Button variant={'outline'} size={'lg'}>
-          <Link to={ROUTES.LOGIN} className="text-primary-foreground text-xl">
-            Sign in
-          </Link>
-        </Button>
-      )}
-      {!isAuthenticated && (
-        <Button variant={'default'} size={'lg'}>
-          <Link
-            to={ROUTES.REGISTRATION}
-            className="text-primary-foreground text-xl"
-          >
-            Sign up
-          </Link>
-        </Button>
+      {isAuthenticated ? (
+        <Link to={ROUTES.PROFILE}>
+          <User
+            className="w-9 h-9 text-primary-foreground"
+            role="img"
+            aria-label="User icon"
+          />
+        </Link>
+      ) : (
+        <>
+          <Button variant={'outline'} size={'lg'}>
+            <Link to={ROUTES.LOGIN} className="text-primary-foreground text-xl">
+              Sign in
+            </Link>
+          </Button>
+          <Button size={'lg'}>
+            <Link
+              to={ROUTES.REGISTRATION}
+              className="text-primary-foreground text-xl"
+            >
+              Sign up
+            </Link>
+          </Button>
+        </>
       )}
       <Link to={ROUTES.CART} className="flex items-center">
         <ShoppingCart
           className="w-9 h-9 text-primary-foreground"
           role="img"
-          aria-label="Cart icon"
+          aria-label="Shopping cart icon"
         />
       </Link>
     </nav>
