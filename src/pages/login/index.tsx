644bcdf2d637667cda6988fb737809b727d50643
Angelina Bezik <angelina1337bz@gmail.com>
2025-06-30 23:56:06 +0300

    feat: implement client-side validation  for login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,58 +1,12 @@
-import { useNavigate } from '@tanstack/react-router';
-import { useState } from 'react';
-
-import { ROUTES } from '@shared/config/routes';
-import { Button } from '@shared/ui/button';
-import { Input } from '@shared/ui/input';
-import { useLogin } from '@shared/viewer/hooks';
+import { LoginForm } from '@pages/login/ui/login-form';
 
 export const Login = (): React.JSX.Element => {
-  const { mutate: login, isPending, error } = useLogin();
-  const navigate = useNavigate();
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    if (!email || !password) return;
-
-    login(
-      { email, password },
-      {
-        onSuccess: () => {
-          navigate({ to: ROUTES.HOME });
-        },
-      }
-    );
-  };
-
   return (
-    <div>
-      <h1 className="text-2xl">Login Page</h1>
-      <form onSubmit={handleSubmit} className="flex gap-2 mb-4 w-3/12">
-        <Input
-          name="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          placeholder="Email"
-        />
-        <Input
-          name="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          placeholder="Password"
-        />
-        {error && <div className="text-red-500 mb-2">{error.message}</div>}
-        <Button type="submit" disabled={isPending} variant="outline">
-          {isPending ? 'Loading…' : 'Login'}
-        </Button>
-      </form>
+    <div className="flex">
+      <div className="flex flex-col gap-6 max-w-md p-8">
+        <h2>Welcome back to YesCode!</h2>
+        <LoginForm />
+      </div>
     </div>
   );
 };
---
+++ src/pages/login/ui/login-form.tsx
@@ -0,0 +1,116 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useNavigate } from '@tanstack/react-router';
+import { Eye, EyeOff } from 'lucide-react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+
+import {
+  type LoginFormType,
+  loginSchema,
+} from '@pages/login/model/validation-schema';
+
+import { ROUTES } from '@shared/config/routes';
+import { Button } from '@shared/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@shared/ui/form';
+import { Input } from '@shared/ui/input';
+import { useLogin } from '@shared/viewer/hooks';
+
+export const LoginForm = (): React.JSX.Element => {
+  const { mutate: login, isPending } = useLogin();
+  const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const form = useForm<LoginFormType>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onChange',
+  });
+
+  const onSubmit = (data: LoginFormType) => {
+    login(data, {
+      onSuccess: () => {
+        navigate({ to: ROUTES.HOME });
+      },
+      onError: (err) => {
+        toast.error(err.message);
+      },
+    });
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Your email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Your password</FormLabel>
+              <div className="relative">
+                <FormControl>
+                  <Input
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Password"
+                    {...field}
+                  />
+                </FormControl>
+                <button
+                  type="button"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                  className="absolute right-2 top-2.5 text-muted-foreground hover:text-foreground transition-all"
+                  tabIndex={-1}
+                >
+                  {showPassword ? (
+                    <EyeOff className="w-5 h-5" />
+                  ) : (
+                    <Eye className="w-5 h-5" />
+                  )}
+                </button>
+              </div>
+              <FormDescription>
+                At least 8 characters with uppercase, lowercase, digit and
+                special character.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button
+          type="submit"
+          className="w-full"
+          disabled={isPending || !form.formState.isValid}
+        >
+          {isPending ? 'Logging in...' : 'Login'}
+        </Button>
+      </form>
+    </Form>
+  );
+};
