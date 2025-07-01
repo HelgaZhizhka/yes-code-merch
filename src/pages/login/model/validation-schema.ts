cb04285a1e58d300780f4c55771a90eb286b283c
Angelina Bezik <angelina1337bz@gmail.com>
2025-07-02 00:43:31 +0300

    refactor: update password validation

--- src/pages/login/model/validation-schema.ts
+++ src/pages/login/model/validation-schema.ts
@@ -8,14 +8,9 @@ export const loginSchema = z.object({
     .email('Invalid email format'),
   password: z
     .string()
-    .min(8, 'Password must be at least 8 characters long')
-    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
-    .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
-    .regex(/\d/, 'Password must contain at least one digit')
-    .regex(/[\W_]/, 'Password must contain at least one special character')
-    .refine((val) => val === val.trim(), {
-      message: 'Password cannot start or end with a space',
-    }),
+    .trim()
+    .min(1, 'Password is required')
+    .min(8, 'Password must be at least 8 characters long'),
 });
 
 export type LoginFormType = z.infer<typeof loginSchema>;
