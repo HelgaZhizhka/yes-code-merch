bab04aa0f5dc83a75bddf9ff05951cbd5a78fcae
HelgaZhizhka <helgazhyzhka@gmail.com>
2025-07-06 21:07:24 +0200

    fix: fix sonar warning

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
 /// <reference types="vitest/config" />
-import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
-import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import viteReact from '@vitejs/plugin-react';
+import { join, dirname as pathDirname, resolve } from 'node:path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { fileURLToPath } from 'node:url';
 const dirname =
   typeof __dirname !== 'undefined'
     ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url));
+    : pathDirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
@@ -29,7 +28,7 @@ export default defineConfig({
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
           storybookTest({
-            configDir: path.join(dirname, '.storybook'),
+            configDir: join(dirname, '.storybook'),
           }),
         ],
         test: {
