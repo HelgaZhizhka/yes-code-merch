643fc5083af2ab02a01afc2c36b32b1e47d0eac3
HelgaZhizhka <helgazhyzhka@gmail.com>
2025-07-06 20:55:50 +0200

    chore: install and settings storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,34 @@
+import { withThemeByClassName } from '@storybook/addon-themes';
+import type { Preview } from '@storybook/react-vite';
+
+import { withStyleDecorator } from './style-decorator';
+
+const preview: Preview = {
+  decorators: [
+    withStyleDecorator,
+    withThemeByClassName({
+      themes: {
+        light: 'light',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }),
+  ],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo',
+    },
+  },
+};
+
+export default preview;
--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 import js from '@eslint/js';
 import importPlugin from 'eslint-plugin-import';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
@@ -129,5 +132,6 @@ export default tseslint.config(
         version: 'detect',
       },
     },
-  }
+  },
+  storybook.configs['flat/recommended']
 );
