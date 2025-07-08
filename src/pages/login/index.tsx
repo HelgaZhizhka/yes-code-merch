7a32acf9e54ce2f8e191fa61453e0e0a05e72d1f
Angelina Bezik <angelina1337bz@gmail.com>
2025-07-08 15:00:37 +0300

    feat: update page layout

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,15 +4,15 @@ import Rectangle from '@shared/assets/Rectangle.png';
 
 export const Login = (): React.JSX.Element => {
   return (
-    <div className="relative flex items-center justify-center flex-col">
-      <div className="flex flex-col gap-6 max-w-md p-8">
+    <div className="relative flex flex-1 items-center justify-center flex-col">
+      <div className="flex flex-col gap-6 max-w-lg p-8 w-full">
         <h2>Welcome back to YesCode!</h2>
         <LoginForm />
       </div>
       <img
         src={Rectangle}
         alt="Raccoon"
-        className="absolute bottom-0 left-0 w-24"
+        className="absolute bottom-0 left-0 w-30"
       />
     </div>
   );
