790acf99a6805e05e9992a460c170843377d96ee
Angelina Bezik <angelina1337bz@gmail.com>
2025-07-17 20:50:30 +0300

    feat: add new routes for new pages

---
+++ src/pages/forgot-password/index.tsx
@@ -0,0 +1,7 @@
+export const ForgotPassword = (): React.JSX.Element => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl mb-4">Forgot password page</h1>
+    </div>
+  );
+};
---
+++ src/pages/reset-password/index.tsx
@@ -0,0 +1,7 @@
+export const ResetPassword = (): React.JSX.Element => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl mb-4">Reset password page</h1>
+    </div>
+  );
+};
