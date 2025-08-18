34e4ba3e60bc990c06373c32daadb1b5b106803f
Angelina Bezik <angelina1337bz@gmail.com>
2025-08-18 21:13:30 +0300

    feat: add shared personal form

--- src/pages/onboarding/ui/init-step.tsx
+++ src/pages/onboarding/ui/init-step.tsx
@@ -1,8 +1,6 @@
 import { Button } from '@shared/ui/button';
 import { Form } from '@shared/ui/form';
-import { FormFieldWrapper } from '@shared/ui/form-field-wrapper';
-import { Input } from '@shared/ui/input';
-import { InputDatepicker } from '@shared/ui/input-datepicker';
+import { PersonalFormFields } from '@shared/ui/personal-form-fields';
 
 import { useFormStep } from '../hooks';
 
@@ -13,66 +11,17 @@ export const InitStep = (): React.JSX.Element => {
     <Form {...form}>
       <form onSubmit={form.handleSubmit(handleNextStep)} className="space-y-6">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 items-start">
-          <FormFieldWrapper
+          <PersonalFormFields
             control={form.control}
-            name="firstName"
-            label="First Name*"
-          >
-            {(field) => (
-              <Input
-                type="text"
-                placeholder="First Name"
-                autoComplete="given-name"
-                {...field}
-              />
-            )}
-          </FormFieldWrapper>
-
-          <FormFieldWrapper
-            control={form.control}
-            name="lastName"
-            label="Last Name*"
-          >
-            {(field) => (
-              <Input
-                type="text"
-                placeholder="Last Name"
-                autoComplete="family-name"
-                {...field}
-              />
-            )}
-          </FormFieldWrapper>
-
-          <FormFieldWrapper
-            control={form.control}
-            name="dateOfBirth"
-            label="Date of Birth*"
-          >
-            {(field) => (
-              <InputDatepicker
-                placeholder="Date of Birth"
-                name="dateOfBirth"
-                value={field.value}
-                onChange={(value) => field.onChange(value)}
-              />
-            )}
-          </FormFieldWrapper>
-
-          <FormFieldWrapper control={form.control} name="phone" label="Phone*">
-            {(field) => <Input type="tel" placeholder="Phone" {...field} />}
-          </FormFieldWrapper>
-
-          <FormFieldWrapper control={form.control} name="title" label="Title">
-            {(field) => <Input type="text" placeholder="Title" {...field} />}
-          </FormFieldWrapper>
-
-          <FormFieldWrapper
-            control={form.control}
-            name="company"
-            label="Company"
-          >
-            {(field) => <Input type="text" placeholder="Company" {...field} />}
-          </FormFieldWrapper>
+            include={[
+              'firstName',
+              'lastName',
+              'dateOfBirth',
+              'phone',
+              'title',
+              'company',
+            ]}
+          />
         </div>
         <Button
           type="submit"
--- src/pages/profile/ui/edit-personal-form.tsx
+++ src/pages/profile/ui/edit-personal-form.tsx
@@ -1,8 +1,7 @@
 import { Button } from '@shared/ui/button';
 import { Form } from '@shared/ui/form';
-import { FormFieldWrapper } from '@shared/ui/form-field-wrapper';
-import { Input } from '@shared/ui/input';
-import { InputDatepicker } from '@shared/ui/input-datepicker';
+
+import { PersonalFormFields } from '@/shared/ui/personal-form-fields';
 
 import { useEditPersonalForm } from '../hooks';
 
@@ -13,63 +12,10 @@ export const EditPersonalForm = (): React.JSX.Element => {
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
         <div className="flex flex-col gap-4">
-          <FormFieldWrapper control={form.control} name="email" label="Email*">
-            {(field) => (
-              <Input
-                type="email"
-                placeholder="Email"
-                autoComplete="email"
-                {...field}
-              />
-            )}
-          </FormFieldWrapper>
-          <FormFieldWrapper
+          <PersonalFormFields
             control={form.control}
-            name="firstName"
-            label="First Name*"
-          >
-            {(field) => (
-              <Input
-                type="text"
-                placeholder="First Name"
-                autoComplete="given-name"
-                {...field}
-              />
-            )}
-          </FormFieldWrapper>
-
-          <FormFieldWrapper
-            control={form.control}
-            name="lastName"
-            label="Last Name*"
-          >
-            {(field) => (
-              <Input
-                type="text"
-                placeholder="Last Name"
-                autoComplete="family-name"
-                {...field}
-              />
-            )}
-          </FormFieldWrapper>
-
-          <FormFieldWrapper
-            control={form.control}
-            name="dateOfBirth"
-            label="Date of Birth*"
-          >
-            {(field) => (
-              <InputDatepicker
-                placeholder="Date of Birth"
-                name="dateOfBirth"
-                value={field.value}
-                onChange={(value) => field.onChange(value)}
-              />
-            )}
-          </FormFieldWrapper>
-          <FormFieldWrapper control={form.control} name="phone" label="Phone*">
-            {(field) => <Input type="tel" placeholder="Phone" {...field} />}
-          </FormFieldWrapper>
+            include={['email', 'firstName', 'lastName', 'dateOfBirth', 'phone']}
+          />
         </div>
         <Button
           type="submit"
