3efcf82ac4029584481d98215d3bb4678ff8a2a1
Angelina Bezik <angelina1337bz@gmail.com>
2025-12-02 19:19:40 +0300

    style: add styles for product list on the catalog page

--- src/entities/product/ui/product-list.tsx
+++ src/entities/product/ui/product-list.tsx
@@ -12,10 +12,10 @@ export const ProductList = ({ products }: ProductListProps) => {
   }
 
   return (
-    <>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
       {products.map((product) => (
         <ProductCard key={product.productId} product={product} />
       ))}
-    </>
+    </div>
   );
 };
