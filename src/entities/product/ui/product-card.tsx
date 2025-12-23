65c662a9a077c91c945dee2f787a3e65ab1c4b14
Angelina Bezik <angelina1337bz@gmail.com>
2025-12-23 17:35:04 +0300

    feat: add dompurify and html-react-parser for description

--- src/entities/product/ui/product-card.tsx
+++ src/entities/product/ui/product-card.tsx
@@ -1,3 +1,6 @@
+import DOMPurify from 'dompurify';
+import parse from 'html-react-parser';
+
 import { AddToCart } from '@features/add-to-cart';
 
 import { Price } from '@shared/ui/price';
@@ -12,6 +15,9 @@ export const ProductCard = ({ product }: ProductCardProps) => {
   const imageUrl =
     product.images?.medium || 'https://placehold.co/400x400?text=No+Image';
 
+  const sanitizedDescription = product.description
+    ? DOMPurify.sanitize(product.description)
+    : null;
   return (
     <div className="flex flex-col gap-4 max-w-xs w-full p-2">
       <div className="w-full relative aspect-[3/4] overflow-hidden shadow-md">
@@ -27,7 +33,9 @@ export const ProductCard = ({ product }: ProductCardProps) => {
         <AddToCart variant="catalog" />
       </div>
       <h3 className="text-2xl font-bold">{product.name}</h3>
-      {product.description && <p className="text-sm">{product.description}</p>}
+      {sanitizedDescription && (
+        <div className="text-sm">{parse(sanitizedDescription)}</div>
+      )}
       <div className="mt-auto">
         <Price value={product.finalPrice} variant="catalog" />
       </div>
